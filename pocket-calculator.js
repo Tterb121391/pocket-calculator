1ac0fd5915ee88d425daa08d6b252adb5e0966d4
Brett Li <wxbml702@gmail.com>
2018-12-19 13:21:07 -0500

    number buttons done

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -1,4 +1,77 @@
+var output = "";
+
+function buttonNum0() {
+  let p = document.getElementById("output");
+  output = output + "0";
+  p.innerHTML = output;
+}
+
 function buttonNum1() {
-  p.document.getElementById("output");
-  p.innerHTML = "1";
+  let p = document.getElementById("output");
+  output = output + "1";
+  p.innerHTML = output;
+}
+
+function buttonNum2() {
+  let p = document.getElementById("output");
+  output = output + "2";
+  p.innerHTML = output;
+}
+
+function buttonNum3() {
+  let p = document.getElementById("output");
+  output = output + "3";
+  p.innerHTML = output;
+}
+
+function buttonNum4() {
+  let p = document.getElementById("output");
+  output = output + "4";
+  p.innerHTML = output;
+}
+
+function buttonNum5() {
+  let p = document.getElementById("output");
+  output = output + "5";
+  p.innerHTML = output;
+}
+
+function buttonNum6() {
+  let p = document.getElementById("output");
+  output = output + "6";
+  p.innerHTML = output;
+}
+
+function buttonNum7() {
+  let p = document.getElementById("output");
+  output = output + "7";
+  p.innerHTML = output;
+}
+
+function buttonNum8() {
+  let p = document.getElementById("output");
+  output = output + "8";
+  p.innerHTML = output;
+}
+
+function buttonNum9() {
+  let p = document.getElementById("output");
+  output = output + "9";
+  p.innerHTML = output;
+}
+
+function add() {
+
+}
+
+function subtract() {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
 }
